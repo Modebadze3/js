0c48fbb6d6fdf5bbdd2eba0a8521d2244073e144
Modebadze3 <72819119+Modebadze3@users.noreply.github.com>
2020-11-03 22:39:48 +0400

    update

--- js.js
+++ js.js
@@ -3,9 +3,16 @@ var user = {
     age: 25,
     gender: 'male'
 };
+// Task1
 if (user.age < 18) {
     console.log('არასრულწლოვანი')
 }
 else if (user.age >= 18 && user.gender == 'male') {
     console.log('სრულწლოვანი ბიჭი')
-};
+}else {
+    console.log('დაფიქსირდა შეცდომა')
+}
+// Task2
+
+let cars = ["audi", "bmw", "lexus", "volkswagen", "ferrary", "porsche"];
+console.log(cars[0]);
